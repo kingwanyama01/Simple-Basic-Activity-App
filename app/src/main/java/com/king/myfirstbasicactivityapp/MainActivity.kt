e104d2a36bf7b0bebeff1078ab47fe88166a9e56
kingwanyama01 <benjamin@emobilis.org>
2020-04-14 11:47:47 +0300

    Fully working simple basic activity app with listeners

---
+++ app/src/main/java/com/king/myfirstbasicactivityapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.king.myfirstbasicactivityapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        mBtnMove.setOnClickListener {
+            intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this,"Settings clicked",Toast.LENGTH_LONG).show()
+                true
+            }
+
+            R.id.action_call -> {
+                Toast.makeText(this,"Call clicked",Toast.LENGTH_LONG).show()
+                true
+            }
+
+
+            R.id.action_sms -> {
+                Toast.makeText(this,"SMS clicked",Toast.LENGTH_LONG).show()
+                true
+            }
+
+            R.id.action_exit -> {
+                Toast.makeText(this,"Exit clicked",Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/king/myfirstbasicactivityapp/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.king.myfirstbasicactivityapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        mBtnBack.setOnClickListener {
+            intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
